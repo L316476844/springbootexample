d900e02c28ed11315eca97eaf59addfadfe89d0a
lvbin <lvbin@oriental-finance.com>
2017-05-22 17:43:18 +0800

    上传文件

--- src/main/java/org/jon/lv/SpringbootexampleApplication.java
+++ src/main/java/org/jon/lv/SpringbootexampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
 import javax.sql.DataSource;
 
 
@@ -31,6 +33,20 @@ public class SpringbootexampleApplication{
 		characterEncodingFilter.setEncoding("UTF-8");
 		characterEncodingFilter.setForceEncoding(true);
 		return characterEncodingFilter;
+	}
+
+	// 文件上传限制
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		// 设置文件大小限制 ,超出设置页面会抛出异常信息，
+		// 这样在文件上传的地方就需要进行异常信息的处理了;
+		factory.setMaxFileSize("2560KB"); // KB,MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("5120KB");
+		// Sets the directory location where files will be stored.
+		// factory.setLocation("路径地址");
+		return factory.createMultipartConfig();
 	}
 
 	//文件下载
